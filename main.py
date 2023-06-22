975930d0176c98cc5e43000cae730607245a7147
Heisenberg1525 <sudaykukreja08@gmail.com>
2023-06-21 21:42:31 -0400

    fixed the clauclate gpa, but math dosn't work yet, bugs remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 academic_grades = {
-    "A+": 4.3,
+    "A+": 4.0,
     "A": 4.0,
     "A-": 3.7,
     "B+": 3.3,
@@ -15,34 +15,34 @@ academic_grades = {
 }
 
 honors_grades = {
-    "A+": 4.945,
-    "A": 4.6,
-    "A-": 4.255,
-    "B+": 3.795,
-    "B": 3.45,
-    "B-": 3.105,
-    "C+": 2.645,
-    "C": 2.3,
-    "C-": 1.955,
-    "D+": 1.338,
-    "D": 1.15,
-    "D-": 0.805,
+    "A+": 4.5,
+    "A": 4.5,
+    "A-": 4.2,
+    "B+": 3.8,
+    "B": 3.5,
+    "B-": 3.2,
+    "C+": 2.8,
+    "C": 2.5,
+    "C-": 2.2,
+    "D+": 1.8,
+    "D": 1.5,
+    "D-": 1.2,
     "F": 0.0
 }
 
 ap_grades = {
-    "A+": 5.375,
+    "A+": 5.0,
     "A": 5.0,
-    "A-": 4.625,
-    "B+": 4.125,
-    "B": 3.75,
-    "B-": 3.375,
-    "C+": 2.875,
-    "C": 2.5,
-    "C-": 2.125,
-    "D+": 1.625,
-    "D": 1.25,
-    "D-": 0.875,
+    "A-": 4.7,
+    "B+": 4.3,
+    "B": 4.0,
+    "B-": 3.7,
+    "C+": 3.3,
+    "C": 3.0,
+    "C-": 2.7,
+    "D+": 2.3,
+    "D": 2.0,
+    "D-": 1.7,
     "F": 0.0
 }
 
@@ -52,7 +52,7 @@ total_grade_points_unweighted = 0.0
 total_credits = 0.0
 
 for i in range(1, num_classes + 1):
-    class_type = input("Enter class type for class {} (Academic, Honors, AP): ".format(i))
+    class_type = input("Enter class type for class {} (Academic, Honors, AP): ")
 
     is_valid_class_type = False
     grade_value = 0.0
@@ -77,7 +77,7 @@ for i in range(1, num_classes + 1):
     credits = float(input("Enter the number of credits for class {}: ".format(i)))
 
     total_grade_points_weighted += grade_value * credits
-    total_grade_points_unweighted += academic_grades.get(class_type.lower(), 0.0) * credits
+    total_grade_points_unweighted += academic_grades.get(grade, 0.0) * credits
     total_credits += credits
 
 weighted_gpa = total_grade_points_weighted / total_credits
